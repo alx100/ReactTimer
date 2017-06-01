2749c795e7ab08681ff003393a78a57e6a50f209
Alx100 <stotsky.alex@gmail.com>
2017-06-01 15:48:49 +0300

    fix jq & foundation karma issues

--- app/app.jsx
+++ app/app.jsx
@@ -8,6 +8,7 @@ var Timer = require('Timer');
 
 // load foundation
 require('style!css!foundation-sites/dist/css/foundation.min.css')
+
 $(document).foundation();
 
 // app css
--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -4,6 +4,8 @@ var CountdownForm = React.createClass({
     onSubmit: function (e) {
         e.preventDefault();
         var strSeconds = this.refs.seconds.value;
+        
+        console.log('inputs count', $('input').length);
 
         if (strSeconds.match(/^[0-9]*$/)) {
             this.refs.seconds.value = '';
